b10592913656fb86424e5ff3cff45d1b97879312
buondevid <davidebriano@gmail.com>
2021-02-08 02:02:53 +0100

    finish winning function

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
-//FACTORY FUNCTION
+// FACTORY FUNCTION
 const Player = (name, mark) => ({ name, mark });
 
-//CREATE objects with factory functions
+// CREATE objects with factory functions
 const human = Player('Davide', 'X');
 const computer = Player('AI', 'O');
 
@@ -11,7 +11,7 @@ const gameboard = (() => {
 	const board = document.querySelector('.grid');
 	let { mark } = human;
 
-	//CHANGE mark
+	// CHANGE mark
 	const changeMark = () => {
 		mark = mark === 'X' ? computer.mark : human.mark;
 	};
@@ -25,21 +25,21 @@ const gameboard = (() => {
 
 	// CREATE - Event listener adding the mark on the board and updating array
 	board.addEventListener('click', (e) => {
-		if (e.target.dataset.pos !== undefined && e.target.textContent === '') {
+		if (e.target.dataset.pos && e.target.textContent === '') {
 			e.target.textContent = mark;
 		}
 		updateArray();
 		changeMark();
 	});
 
-	//READ array - UPDATE grid
+	// READ array - UPDATE grid
 	const renderArray = () => {
 		for (let i = 0; i < gridArray.length; i++) {
 			document.querySelector(`div[data-pos='${i}']`).textContent = gridArray[i];
 		}
 	};
 
-	//RESET array and grid
+	// RESET array and grid
 	const resetArrayGrid = () => {
 		for (let i = 0; i < gridArray.length; i++) {
 			document.querySelector(`div[data-pos='${i}']`).textContent = '';
@@ -47,7 +47,7 @@ const gameboard = (() => {
 		}
 	};
 
-	//INIT
+	// INIT
 	// renderArray();
 
 	return {
@@ -61,28 +61,31 @@ const gameboard = (() => {
 const game = (() => {
 	const restartButton = document.getElementById('restart');
 
-	//event listeners
+	// event listeners
 	restartButton.addEventListener('click', gameboard.resetArrayGrid);
 
-	//CHECK winning condition
-	const checkWinner = (array) => {
-		if (array[0] === array[1] === array[2]
-				|| array[3] === array[4] === array[5]
-				|| array[6] === array[7] === array[8]
-				|| array[0] === array[3] === array[6]
-				|| array[1] === array[4] === array[7]
-				|| array[2] === array[5] === array[8]
-				|| array[0] === array[4] === array[8]
-				|| array[2] === array[4] === array[6])
-			 return 'win';
+	// CHECK winning condition
+	const checkWinOrTie = (mapArray) => {
+		const array = mapArray.map((x, i) => (x === '') ? (x = i) : (x = x)); // replace "" array elements with numbers
+		if ((array[0] === array[1] && array[1] === array[2])
+				|| (array[3] === array[4] && array[4] === array[5])
+				|| (array[6] === array[7] && array[7] === array[8])
+				|| (array[0] === array[3] && array[3] === array[6])
+				|| (array[1] === array[4] && array[4] === array[7])
+				|| (array[2] === array[5] && array[5] === array[8])
+				|| (array[0] === array[4] && array[4] === array[8])
+				|| (array[2] === array[4] && array[4] === array[6]))
+			return console.log('win');
+		if (!(mapArray.includes(''))) console.log('tie');
 	};
 
 	const declareWinner = (win) => {
 		if (win !== 'win') return;
-		else console.log('winner')
-	}
+		console.log('winner');
+	};
 
 	return {
-
+		checkWinOrTie,
+		declareWinner,
 	};
 })();
